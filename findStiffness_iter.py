31def4e96e333a66509daaca16c1b186d3053864
Gavin Day <mngad@leeds.ac.uk>
2017-03-03 11:19:07 +0000

    updated to work with new findStiffness.py

--- findStiffness_iter.py
+++ findStiffness_iter.py
@@ -5,7 +5,7 @@ stiffnesslist = []
 i=100
 incrSize =1
 while i > 0:
-    stiffnesslist.append(fs.findStiffness(i,incrSize))
+    stiffnesslist.append(fs.findStiffness(i,incrSize,False,'M:\HT_Compression_All\HT_Compression_3+post+frozen__BETTER_NAMING'))
     i = i-1
     pc.copy(''.join(stiffnesslist))
 
